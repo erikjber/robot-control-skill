7eab7398f4a72957318bdb2c8677e982678bba87
Erik Berglund <erik.berglund@gmail.com>
2021-03-07 20:45:15 +0100

    First working version.

--- __init__.py
+++ __init__.py
@@ -1,15 +1,47 @@
 from mycroft import MycroftSkill, intent_file_handler
-
+from pybotvac import Robot
+from mycroft.util import LOG
+import json
 
 class RobotControl(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
+        with open("NEATOROBOT") as f:
+            lines = f.read().split(",")
+            self.robot = Robot(lines[0],lines[1],lines[2])
 
-    @intent_file_handler('control.robot.intent')
-    def handle_control_robot(self, message):
-        self.speak_dialog('control.robot')
+    @intent_file_handler('control.robot.locate')
+    def handle_locate_robot(self, message):
+        res = self.robot.locate()
+        parsed = json.loads(res.text)
+        if parsed['result'] == 'command_rejected':
+            self.speak_dialog('control.robot.unable')
 
+
+    @intent_file_handler('control.robot.start')
+    def handle_start_robot(self, message):
+        res = self.robot.start_cleaning()
+        parsed = json.loads(res.text)
+        if parsed['result'] == 'command_rejected':
+            self.speak_dialog('control.robot.unable')
+
+
+    @intent_file_handler('control.robot.stop')
+    def handle_stop_robot(self, message):
+        res = self.robot.stop_cleaning()
+        parsed = json.loads(res.text)
+        if parsed['result'] == 'command_rejected':
+            self.speak_dialog('control.robot.unable')
+
+
+    @intent_file_handler('control.robot.home')
+    def handle_park_robot(self, message):
+        res = self.robot.send_to_base()
+        parsed = json.loads(res.text)
+        if parsed['result'] == 'command_rejected':
+            self.speak_dialog('control.robot.unable')
 
 def create_skill():
+    LOG.info("RobotControl.create_skill called")
     return RobotControl()
 
