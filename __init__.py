f93ee984dac5391e0ac93346fec3b2b054323eb6
Erik Berglund <erik.berglund@gmail.com>
2023-05-02 11:21:57 +0000

    Make skill work even if network is not ready when it loads. Added requriements.txt.

--- __init__.py
+++ __init__.py
@@ -2,16 +2,31 @@ from mycroft import MycroftSkill, intent_file_handler
 from pybotvac import Robot
 from mycroft.util import LOG
 import json
+from os.path import dirname, join
 
 class RobotControl(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
-        with open("NEATOROBOT") as f:
+        auth_path = join(dirname(__file__),"NEATOROBOT")
+        with open(auth_path) as f:
             lines = f.read().split(",")
-            self.robot = Robot(lines[0],lines[1],lines[2])
+            self.serial = lines[0]
+            self.secret = lines[1]
+            self.traits = lines[2]
+        self.robot = None
+        self._init_robot()
+
+
+    def _init_robot(self):
+        try:
+            self.robot = Robot(self.serial,self.secret,self.traits)
+        except:
+            self.robot = None
 
     @intent_file_handler('control.robot.locate')
     def handle_locate_robot(self, message):
+        if not self.robot:
+            self._init_robot()
         res = self.robot.locate()
         parsed = json.loads(res.text)
         if parsed['result'] == 'command_rejected':
@@ -20,6 +35,8 @@ class RobotControl(MycroftSkill):
 
     @intent_file_handler('control.robot.start')
     def handle_start_robot(self, message):
+        if not self.robot:
+            self._init_robot()
         res = self.robot.start_cleaning(category=4)
         parsed = json.loads(res.text)
         if parsed['result'] == 'command_rejected':
@@ -28,6 +45,8 @@ class RobotControl(MycroftSkill):
 
     @intent_file_handler('control.robot.stop')
     def handle_stop_robot(self, message):
+        if not self.robot:
+            self._init_robot()
         res = self.robot.stop_cleaning()
         parsed = json.loads(res.text)
         if parsed['result'] == 'command_rejected':
@@ -36,6 +55,8 @@ class RobotControl(MycroftSkill):
 
     @intent_file_handler('control.robot.home')
     def handle_park_robot(self, message):
+        if not self.robot:
+            self._init_robot()
         res = self.robot.send_to_base()
         parsed = json.loads(res.text)
         if parsed['result'] == 'command_rejected':
